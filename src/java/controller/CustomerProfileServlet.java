f47f0c3573377288b69ba32f91b16830681bfd64
DuyAnh953 <duy021101@gmail.com>
2025-10-09 23:03:52 +0700

    Create profile page for customer

---
+++ src/java/controller/CustomerProfileServlet.java
@@ -0,0 +1,107 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package controller;
+
+import dal.UserDAO;
+import entity.User;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.sql.Date;
+
+/**
+ *
+ * @author leanh
+ */
+@WebServlet(name = "CustomerProfileServlet", urlPatterns = {"/CustomerProfile"})
+public class CustomerProfileServlet extends HttpServlet {
+   private UserDAO userDAO = new UserDAO();
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet CustomerProfileServlet</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet CustomerProfileServlet at " + request.getContextPath () + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    } 
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Simulate logged-in user
+        String email = "customer1@gmail.com";  // pretend cause no login for now
+        User user = userDAO.getUserByEmail(email);
+        request.setAttribute("user", user);
+        request.getRequestDispatcher("profile.jsp").forward(request, response);
+
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        int userId = Integer.parseInt(request.getParameter("userId"));
+        String fullName = request.getParameter("fullName");
+        String phone = request.getParameter("phone");
+        String gender = request.getParameter("gender");
+        String dob = request.getParameter("dob");
+        String address = request.getParameter("address");
+
+        User updatedUser = new User();
+        updatedUser.setUserID(userId);
+        updatedUser.setFullName(fullName);
+        updatedUser.setPhoneNumber(phone);
+        updatedUser.setGender(gender);
+        updatedUser.setDateOfBirth(Date.valueOf(dob));
+        updatedUser.setAddress(address);
+
+        boolean success = userDAO.updateUser(updatedUser);
+
+        if (success) {
+            response.sendRedirect("CustomerProfile");
+        } else {
+            request.setAttribute("error", "Cập nhật thất bại!");
+            request.getRequestDispatcher("profile.jsp").forward(request, response);
+        }
+    }
+    
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
